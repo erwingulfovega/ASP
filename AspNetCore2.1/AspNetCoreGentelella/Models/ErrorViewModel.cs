a5ebe8c1e268ee256bc0c948d91812d8e9efd487
Erwin Gulfo Vega <ermiguve24@gmail.com>
2022-08-27 11:01:04 -0500

    Ejercicios POO C#

---
+++ AspNetCore2.1/AspNetCoreGentelella/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreGentelella.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ fundamentosCsharp/ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
+
+
+
